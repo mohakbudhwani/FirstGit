0f674e051333f31555090de47286f583e8133453
mohakbudhwani <mohakbudhwani1999>
2024-02-16 19:16:32 +0530

    task1

---
+++ task1/task1.js
@@ -0,0 +1,14 @@
+const mainheading=document.getElementById("main-heading");
+mainheading.textContent="Fruit World"
+
+const header=document.getElementById("header");
+header.style.color="orange"
+header.style.backgroundColor="green"
+header.style.borderBottom="solid orange"
+
+
+const basketheading=document.getElementById("basket-heading");
+basketheading.style.color="green"
+
+const thanks=document.getElementById("thanks");
+thanks.innerHTML='<p>Please visit us again.</p>
